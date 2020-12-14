993064bf45774f5d976c0f75b2c897c6adfbd398
Usman <mhm.usman@gmail.com>
2020-12-14 16:54:40 +0100

    uploaded simple script for soap request

---
+++ kommunewebservice-soap.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from zeep import Client
+from zeep.transports import Transport
+from zeep.helpers import serialize_object
+
+import getpass
+
+def get_user_and_password():
+    user=input('Oppgi brukernavn: ')
+    password=getpass.getpass('Oppgi passord: ')
+    return user,password
+
+def get_matrikkelcontext(sosi_kode):
+    return {'sosiKode':sosi_kode,'matrikkelKlientversjon':'3.18.0.25'}
+
+
+def get_kommuner(wsdl,binding):
+    client=Client(wsdl, transport=Transport(session=session))
+    service=client.bind(binding)
+    response=service.findKommuner(
+        matrikkelContext=get_matrikkelcontext(sosi_kode=23))
+    kommuner=serialize_object(response)
+    print(kommuner)
+    return response
+
+def parse_kommuneresponse(ws_reponse):
+    kommuner=serialize_object(ws_reponse)
+    kommuner = [i for i in kommuner if (i['gyldigTilDato'] is None)]
+    return kommuner
+
+
+wsdl='https://www.nd.matrikkel.no/innsynapi_v3/kommune/KommuneWebService?WSDL'
+binding='KommuneWebService'
+
+user,password=get_user_and_password()
+
+session = Session()
+
+session.auth = HTTPBasicAuth(user, password)
+
+kommuner=get_kommuner(wsdl,binding)
+
+
